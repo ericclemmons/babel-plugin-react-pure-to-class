225a67460edc9b2803f985c5ec0150ba222dac5c
Eric Clemmons <eric@smarterspam.com>
2016-02-13 10:55:53 -0600

    Class-based demo

---
+++ example/webpack.config.babel.js
@@ -0,0 +1,47 @@
+import path from "path";
+
+export default {
+  entry: {
+    client: [
+      "./src/client.js",
+    ]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel",
+        query: {
+          babelrc: false,
+          cacheDirectory: true,
+          passPerPreset: true,
+          presets: [
+            "react",
+            "es2015",
+            "stage-0",
+          ],
+          plugins: [
+            ["react-transform", {
+              transforms: [{
+                transform: "react-transform-hmr",
+                imports: ["react"],
+                locals: ["module"],
+              }],
+            }],
+          ],
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  output: {
+    chunkFilename: "[id].[hash:5]-[chunkhash:7].js",
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+    filename: "[name].js",
+    libraryTarget: "var",
+    path: path.join(__dirname, "build/client"),
+    publicPath: "/",
+  },
+};
